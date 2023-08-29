10ad5dce9a77673312bf28cc2e977d5f43ced673
dpanhey <d.panhey@gmx.de>
2023-08-29 22:52:23 +0200

    added reference to users for posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_195904) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_205025) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "users_id"
+    t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
